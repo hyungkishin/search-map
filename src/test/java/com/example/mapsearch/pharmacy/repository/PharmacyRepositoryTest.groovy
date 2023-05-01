8bb3833b7374220507991dd4aab9b87fb7c31dc6
hyungkishin <tkaqkeldk@nate.com>
2023-05-01 21:52:10 +0900

    feat: 통합 테스트 환경 구성

---
+++ src/test/java/com/example/mapsearch/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -0,0 +1,33 @@
+package com.example.mapsearch.pharmacy.repository
+
+import com.example.mapsearch.AbstractIntegrationContainerBaseTest
+import com.example.mapsearch.pharmacy.entity.Pharmacy
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+
+class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
+
+    @Autowired
+    private PharmacyRepository pharmacyRepository;
+
+    @Test
+    void testSavePharmacy() {
+        // Given
+        Pharmacy pharmacy = Pharmacy.builder()
+                .pharmacyName("Test Pharmacy")
+                .pharmacyAddress("123 Main St")
+                .latitude(40.7128)
+                .longitude(74.0060)
+                .build();
+
+        // When
+        pharmacyRepository.save(pharmacy);
+
+        // Then
+        Pharmacy savedPharmacy = pharmacyRepository.findById(pharmacy.getId()).orElse(null);
+//        assertThat(savedPharmacy).isNotNull();
+        assertThat(savedPharmacy.getPharmacyName()).isEqualTo("Test Pharmacy");
+    }
+
+
+}
